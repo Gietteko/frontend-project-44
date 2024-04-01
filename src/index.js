59e7f2cbe7f72b9052f58d7c56fa2f6b7c9efd4e
gietteko <tagil201328@gmail.com>
2024-04-01 17:25:45 +0300

    stat 6step

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+export const getRandomNum = () => {
+  const max = 100;
+  const min = 0;
+  const randomNum = Math.floor(Math.random() * (max - min)) + min;
+  return randomNum;
+};
+
+export const parityTest = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNum1 = getRandomNum();
+    console.log(`Question: ${randomNum1}`);
+    let rigthAnswer;
+
+    if (randomNum1 % 2 === 0) { // привязываем правильные ответы к случайному числу до ответа игрока
+      rigthAnswer = 'yes';
+    } else if (randomNum1 % 2 !== 0) {
+      rigthAnswer = 'no';
+    }
+
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === rigthAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer:(. Correct answer was '${rigthAnswer}'.\nLet's try again, ${name}!`);
+      break;
+    }
+    if (i === 3) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
