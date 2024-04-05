3550d25f35ab1a75951b66936101c63f551e57a5
gietteko <tagil201328@gmail.com>
2024-04-05 12:01:55 +0300

    step 9

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { prime as app } from '../src/index.js';
+
+app();
--- src/index.js
+++ src/index.js
@@ -71,6 +71,22 @@ const getNewArr = (arr, index) => {
   }
   return newArr;
 };
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 export const parityTest = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
@@ -168,6 +184,35 @@ export const progression = () => {
       console.log('Correct!');
     } else {
       console.log(`'${valueOfPlayer}' is wrong answer:(. Correct answer was '${rigthAnswer}'.\nLet's try again, ${name}!`);
+      break;
+    }
+    if (i === 3) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
+
+export const prime = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNum = getRandomNum();
+    const randomNumIsPrime = isPrime(randomNum);
+    console.log(`Question: ${randomNum}`);
+    let rigthAnswer;
+
+    if (randomNumIsPrime === true) {
+      rigthAnswer = 'yes';
+    } else if (randomNumIsPrime === false) {
+      rigthAnswer = 'no';
+    }
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === rigthAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer:(. Correct answer was '${rigthAnswer}'.\nLet's try again, ${name}!`);
       break;
     }
     if (i === 3) {
